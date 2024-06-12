ac2d9b8b8cb371063fc621b872f51d7c5c530d9d
John Paul Quintana <jpquintana2024@gmail.com>
2024-06-13 06:59:25 +0800

    updates on game

--- resources/views/admin/game.blade.php
+++ resources/views/admin/game.blade.php
@@ -114,7 +114,7 @@
                 <div class="p-2 text-gray-900 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2">
                     @foreach ($sport->sportsCategories as $c)
                         <!-- Card -->
-                        <a class="group flex flex-col shadow border border-slate-100 bg-slate-100 rounded-sm hover:shadow-md transition dark:bg-neutral-900 dark:border-neutral-800" href="#">
+                        <a data-id="{{ $c->id }}" class="setPlayer group flex flex-col shadow border border-slate-100 bg-slate-100 rounded-sm hover:shadow-md transition dark:bg-neutral-900 dark:border-neutral-800 hover:cursor-pointer">
                             <div class="p-4">
                                 <div class="flex justify-between items-center">
                                     <div class="flex items-center gap-1">
@@ -151,4 +151,43 @@
             </div>
         </div>
     </div>
+
+    @include('admin.game.team')
+
+    @section('scripts')
+        <script>
+            let teams = @json($sport->teams);
+            // console.log(teams)
+            // console.log('conected')
+            $(document).ready(function(){
+                $('.setPlayer').click(function(){
+                    renderTeams(teams)
+                    let categoryId = $(this).data('id')
+                    $('#category_id').val(categoryId)
+                    $('#openModalGame').trigger('click')
+                })
+            })
+
+            const renderTeams = (teams)=>{
+                let team = ''
+                let teamProfile = "{{ asset('storage') }}"
+                teams.forEach(t => {
+                    team += `
+                        <a class="group flex flex-col bg-white shadow-sm rounded-xl hover:shadow-md transition dark:bg-neutral-900 dark:border-neutral-800" href="#">
+                        <label class="flex items-center gap-2 p-3 w-full bg-slate-100 border border-slate-100 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 group-hover:cursor-pointer">
+                            <input type="checkbox" name="selectedTeams[]" value="${t.id}" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
+                            <div class="flex items-center gap-1">
+                            <img class="w-[60px] h-[50px] rounded-md" src="${teamProfile}/${t.profile}" alt="">
+                            <span class="font-bold">${t.team_name}</span>
+                            </div>
+                        </label>
+                        
+                        </a>
+                    `
+                });
+
+                $('#renderTeams').html(team)
+            }
+        </script>
+    @endsection
 </x-app-layout>
